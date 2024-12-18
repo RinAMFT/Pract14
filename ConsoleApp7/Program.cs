f598ba2a80ff0e878501f2df840c1fd6221d2afb
RinAMFT <mucurkaarina@gmail.com>
2024-12-18 09:22:06 +0200

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        static void Func()
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("Hi!");
+            }
+        }
+    }
+}
