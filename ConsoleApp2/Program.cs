0f774d04cf541c32b2d4a03bc6622719de46fae2
RinAMFT <mucurkaarina@gmail.com>
2024-12-18 09:14:37 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Input("n=");
+            Func(n);
+            Console.ReadKey();
+        }
+        static int Input(string messege)
+        {
+            Console.Write(messege);
+            return int.Parse(Console.ReadLine());
+             
+        }
+        static void Func(int n)
+        {
+            for (int x = 1; x <= n; x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
